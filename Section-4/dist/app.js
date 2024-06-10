e0331d62d226979e38f4651b5df700e045540c48
kuboyumi <kuboyumi2@gmail.com>
2024-06-10 22:23:30 +0900

    feat: ~54

--- Section-4/dist/app.js
+++ Section-4/dist/app.js
@@ -1,2 +1,20 @@
 "use strict";
+const userName = 'Max';
+let age = 30;
+age = 29;
+const hobbies = ['Sports', 'Cooking'];
+const activeHobbies = ['Hiking'];
+activeHobbies.push(...hobbies);
+const person = {
+    name: 'Max',
+    age: 30
+};
+const copiedPerson = Object.assign({}, person);
+const add = (...numbers) => {
+    return numbers.reduce((curResult, curValue) => {
+        return curResult + curValue;
+    }, 0);
+};
+const addedNumbers = add(5, 10, 2, 3.7);
+console.log(addedNumbers);
 //# sourceMappingURL=app.js.map
