7e2727d284c0628dfc44eede581e9ac518cd20ca
kuboyumi <kuboyumi2@gmail.com>
2024-06-12 21:43:05 +0900

    feat: ~68

--- Section-5/dist/app.js
+++ Section-5/dist/app.js
@@ -1,14 +1,72 @@
 "use strict";
 class Department {
-    constructor(n) {
-        this.name = n;
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+        this.employees = [];
     }
     describe() {
-        console.log('Department: ' + this.name);
+        console.log(`Department (${this.id}): ${this.name}`);
+    }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
     }
 }
-const accounting = new Department('Accounting');
-accounting.describe();
-const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
-accountingCopy.describe();
+class ITDepartment extends Department {
+    constructor(id, admins) {
+        super(id, 'IT');
+        this.admins = admins;
+    }
+}
+class AccountingDepartment extends Department {
+    get mostRecentReport() {
+        if (this.lastReport) {
+            return this.lastReport;
+        }
+        throw new Error('No report found.');
+    }
+    set mostRecentReport(value) {
+        if (!value) {
+            throw new Error('Please pass in a valid value!');
+        }
+        this.addReport(value);
+    }
+    constructor(id, reports) {
+        super(id, 'Accounting');
+        this.reports = reports;
+        this.lastReport = reports[0];
+    }
+    addEmployee(name) {
+        if (name === 'Max') {
+            return;
+        }
+        this.employees.push(name);
+    }
+    addReport(text) {
+        this.reports.push(text);
+        this.lastReport = text;
+    }
+    printReports() {
+        console.log(this.reports);
+    }
+}
+const it = new ITDepartment('d1', ['Max']);
+it.addEmployee('Max');
+it.addEmployee('Manu');
+it.describe();
+it.name = 'NEW NAME';
+it.printEmployeeInformation();
+console.log(it);
+const accounting = new AccountingDepartment('d2', []);
+accounting.mostRecentReport = 'Year End Report';
+accounting.addReport('Something went wrong...');
+console.log(accounting.mostRecentReport);
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+accounting.printReports();
+accounting.printEmployeeInformation();
 //# sourceMappingURL=app.js.map
