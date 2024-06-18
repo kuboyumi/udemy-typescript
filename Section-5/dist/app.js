e46f693c298fbc00bb4f2b8dae87dff87e2d9c72
kuboyumi <kuboyumi2@gmail.com>
2024-06-19 01:34:26 +0900

    feat: ~78

--- Section-5/dist/app.js
+++ Section-5/dist/app.js
@@ -1,11 +1,19 @@
 "use strict";
-let user1;
-user1 = {
-    name: 'Max',
-    age: 30,
+let add;
+add = (n1, n2) => {
+    return n1 + n2;
+};
+class Person {
+    constructor(n) {
+        this.age = 30;
+        this.name = n;
+    }
     greet(phrase) {
         console.log(phrase + ' ' + this.name);
     }
-};
+}
+let user1;
+user1 = new Person('Max');
 user1.greet('Hi there - I am');
+console.log(user1);
 //# sourceMappingURL=app.js.map
