75435a5c31c3bbc3308bb04ee51c02c77ac34571
kuboyumi <kuboyumi2@gmail.com>
2024-06-22 16:17:06 +0900

    feat: ~93

--- Section-6/dist/app.js
+++ Section-6/dist/app.js
@@ -1,4 +1,5 @@
 "use strict";
+var _a;
 const e1 = {
     name: 'Max',
     privileges: ['create-server'],
@@ -10,37 +11,15 @@ function add(a, b) {
     }
     return a + b;
 }
-function printEmployeeInformation(emp) {
-    console.log('Name: ' + emp.name);
-    if ('privileges' in emp) {
-        console.log('Privileges: ' + emp.privileges);
-    }
-    if ('startDate' in emp) {
-        console.log('Start Date: ' + emp.startDate);
-    }
-}
-printEmployeeInformation({ name: 'Manu', startDate: new Date() });
-class Car {
-    drive() {
-        console.log('Driving...');
-    }
-}
-class Truck {
-    drive() {
-        console.log('Driving a truck...');
-    }
-    loadCargo(amount) {
-        console.log('Loading cargo ...' + amount);
-    }
-}
-const v1 = new Car();
-const v2 = new Truck();
-function useVehicle(vehicle) {
-    vehicle.drive();
-    if (vehicle instanceof Truck) {
-        vehicle.loadCargo(1000);
-    }
-}
-useVehicle(v1);
-useVehicle(v2);
+const result = add('Max', ' Schwarz');
+result.split(' ');
+const fetchedUserData = {
+    id: 'u1',
+    name: 'Max',
+    job: { title: 'CEO', description: 'My own company' }
+};
+console.log((_a = fetchedUserData === null || fetchedUserData === void 0 ? void 0 : fetchedUserData.job) === null || _a === void 0 ? void 0 : _a.title);
+const userInput = undefined;
+const storedData = userInput !== null && userInput !== void 0 ? userInput : 'DEFAULT';
+console.log(storedData);
 //# sourceMappingURL=app.js.map
